70f7631f220d003e5ab573777015de49f62c2d9c
francescodilillo <france.dilillo@gmail.com>
2020-05-28 23:02:46 +0200

    Beginning of the script

---
+++ 15 - POST/index.py
@@ -0,0 +1,42 @@
+################################################################################
+#
+# Author: francescodilillo
+# Purpose: Create web requests handler for get and post
+# Last Edit Date: 28-05-2020
+#
+#
+################################################################################
+
+import tornado.web                  # it contains the handlers
+import tornado.ioloop               # it contains the thread that will wait for results
+import asyncio
+import json
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())     # python-3.8.0a4 / Windows
+
+
+class queryRequestHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        fh = open("numbers.txt")
+        numbers = fh.read().splitlines()
+        fh.close()
+        self.write(json.dumps(numbers))
+
+    def post(self):
+        num = self.get_argument('num')
+        fh = open("numbers.txt", "a")
+        fh.write(f'{num}\n')
+        fh.close()
+        self.write(json.dumps("Number added to the list"))
+
+
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/list", queryRequestHandler)
+    ])
+
+port = 8882
+app.listen(port)
+print("Application is listening on port", port)
+tornado.ioloop.IOLoop.current().start()
